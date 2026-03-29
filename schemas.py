af4a9a6f257472c86fb61c444884c1c6875d1f8f
Gautam291323 <pardeshigautam291@gmail.com>
2026-03-29 12:27:47 +0530

    remove changes

--- schemas.py
+++ schemas.py
@@ -4,18 +4,8 @@ class AddressCreate(BaseModel):
     name: str
     phone: str
     address: str
-    latitude: float
-    longitude: float
-
-class AddressOut(AddressCreate):
-    id: int
-
-    class Config:
-        from_attributes = True
 
 class AddressUpdate(BaseModel):
     name: str | None = None
     phone: str | None = None
     address: str | None = None
-    latitude: float | None = None
-    longitude: float | None = None
